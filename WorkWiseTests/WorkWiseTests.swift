b4ac94378ccd539ab6397a739603b422d9041585
Sravya Bayaneni <sravya.bayaneni@gmail.com>
2025-11-09 16:48:14 -0800

    Initial commit (reset again)

---
+++ WorkWiseTests/WorkWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWiseTests.swift
+//  WorkWiseTests
+//
+//  Created by Sravya Bayaneni on 11/8/25.
+//
+
+import Testing
+@testable import WorkWise
+
+struct WorkWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
